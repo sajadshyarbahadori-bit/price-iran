96786a2348a2f7157ca1943e84fa95fa3fa3f516
sajadshyarbahadori-bit <sajadshyarbahadori@gmail.com>
2026-08-22 00:51:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-async function updatePrices() {
-    alert("تابع اجرا شد!");
-
-    try {
-        const response = await fetch("https://api.persiantoolbox.ir/market");
-        alert("پاسخ API: " + response.status);
-
-        const data = await response.json();
-        alert(JSON.stringify(data).slice(0, 500));
-
-    } catch (error) {
-        alert("خطا: " + error.message);
-    }
-}
-
-updatePrices();
+alert("TEST 123");
