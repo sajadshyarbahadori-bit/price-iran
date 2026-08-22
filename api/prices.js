e6c769e1273a57f9a7b41515d4eae34b8ce8c485
sajadshyarbahadori-bit <sajadshyarbahadori@gmail.com>
2026-08-22 14:14:05 +0330

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,64 +1,17 @@
-module.exports = (req, res) => {
-    res.status(200).json([
+```javascript
+export default function handler(request) {
+    return new Response(
+        JSON.stringify({
+            ok: true,
+            message: "API WORKS"
+        }),
         {
-            code: "GOLD_18_RLS",
-            value: 206620235
-        },
-        {
-            code: "GOLD_24_RLS",
-            value: 275802000
-        },
-        {
-            code: "GOLD_MESGHAL_RLS",
-            value: 895030000
-        },
-        {
-            code: "SEKKEH_RLS",
-            value: 2052525000
-        },
-        {
-            code: "BAHAR_RLS",
-            value: 2011825000
-        },
-        {
-            code: "NIM_SEKKEH_RLS",
-            value: 1042500000
-        },
-        {
-            code: "ROB_SEKKEH_RLS",
-            value: 571875000
-        },
-        {
-            code: "GERAMI_SEKKEH_RLS",
-            value: 296666667
-        },
-        {
-            code: "USD_RLS",
-            value: 1914000
-        },
-        {
-            code: "EUR_RLS",
-            value: 2236400
-        },
-        {
-            code: "GBP_RLS",
-            value: 2604833
-        },
-        {
-            code: "AED_RLS",
-            value: 520410
-        },
-        {
-            code: "TRY_RLS",
-            value: 39883
-        },
-        {
-            code: "GOLD_OUNCE_USD",
-            value: 4596.46
-        },
-        {
-            code: "SILVER_OUNCE_USD",
-            value: 68.965
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
         }
-    ]);
-};
+    );
+}
+```
+
