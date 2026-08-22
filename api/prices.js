75c0412d08bbda2356b908586af9f1fdf9e2d3d3
sajadshyarbahadori-bit <sajadshyarbahadori@gmail.com>
2026-08-22 14:12:15 +0330

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,39 +1,64 @@
-```javascript
-export default async function handler(request) {
-    try {
-        const response = await fetch(
-            "https://api.oanor.com/irr-api/v1/gold",
-            {
-                headers: {
-                    "x-oanor-key": process.env.OANOR_API_KEY,
-                    "Accept": "application/json"
-                }
-            }
-        );
-
-        const data = await response.text();
-
-        return new Response(data, {
-            status: response.status,
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            }
-        });
-
-    } catch (error) {
-        return new Response(
-            JSON.stringify({
-                error: "API request failed",
-                message: error.message
-            }),
-            {
-                status: 500,
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }
-        );
-    }
-}
-```
+module.exports = (req, res) => {
+    res.status(200).json([
+        {
+            code: "GOLD_18_RLS",
+            value: 206620235
+        },
+        {
+            code: "GOLD_24_RLS",
+            value: 275802000
+        },
+        {
+            code: "GOLD_MESGHAL_RLS",
+            value: 895030000
+        },
+        {
+            code: "SEKKEH_RLS",
+            value: 2052525000
+        },
+        {
+            code: "BAHAR_RLS",
+            value: 2011825000
+        },
+        {
+            code: "NIM_SEKKEH_RLS",
+            value: 1042500000
+        },
+        {
+            code: "ROB_SEKKEH_RLS",
+            value: 571875000
+        },
+        {
+            code: "GERAMI_SEKKEH_RLS",
+            value: 296666667
+        },
+        {
+            code: "USD_RLS",
+            value: 1914000
+        },
+        {
+            code: "EUR_RLS",
+            value: 2236400
+        },
+        {
+            code: "GBP_RLS",
+            value: 2604833
+        },
+        {
+            code: "AED_RLS",
+            value: 520410
+        },
+        {
+            code: "TRY_RLS",
+            value: 39883
+        },
+        {
+            code: "GOLD_OUNCE_USD",
+            value: 4596.46
+        },
+        {
+            code: "SILVER_OUNCE_USD",
+            value: 68.965
+        }
+    ]);
+};
