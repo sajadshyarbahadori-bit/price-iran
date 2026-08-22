e535aea3cf289fe07cf562c47814b8bc8b1b6c21
sajadshyarbahadori-bit <sajadshyarbahadori@gmail.com>
2026-08-22 14:19:00 +0330

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,6 +1,29 @@
-module.exports = async function handler(req, res) {
-    res.status(200).json({
-        success: true,
-        message: "API WORKS"
-    });
+module.exports = async (req, res) => {
+    try {
+        const response = await fetch(
+            "https://api.oanor.com/irr-api/v1/gold",
+            {
+                headers: {
+                    "x-oanor-key": process.env.OANOR_API_KEY,
+                    "Accept": "application/json"
+                }
+            }
+        );
+
+        const data = await response.text();
+
+        res.status(response.status);
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
+        res.send(data);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            error: "API ERROR",
+            message: error.message
+        });
+    }
 };
